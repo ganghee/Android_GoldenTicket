b9eeb748febc080b2808e90f32f5ad6c9513b8b6
ganghee <idiosauri@naver.com>
2019-08-05 23:53:21 +0900

    add resource

--- app/src/main/java/com/dazzi/goldenticket/data/LotteryConfirmVPData.kt
+++
@@ -1,6 +0,0 @@
-package com.dazzi.goldenticket.data
-
-data class LotteryConfirmVPData (
-    var title: String,
-    var time: String
-)
---
+++ app/src/main/java/com/dazzi/goldenticket/data/ShowTodayData.kt
@@ -0,0 +1,9 @@
+package com.dazzi.goldenticket.data
+
+data class ShowData(
+    val show_idx: Int,
+    val image_url: String,
+    val name: String,
+    val location: String,
+    val running_time: String
+)
