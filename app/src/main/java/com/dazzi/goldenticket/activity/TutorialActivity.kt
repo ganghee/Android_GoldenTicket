4d7dcdbea6fca09f2cf1c4ba79832b09e67a2105
ganghee <idiosauri@naver.com>
2019-08-08 20:47:02 +0900

    update for clean code

--- app/src/main/java/com/dazzi/goldenticket/activity/TutorialActivity.kt
+++ app/src/main/java/com/dazzi/goldenticket/activity/TutorialActivity.kt
@@ -1,7 +1,7 @@
 package com.dazzi.goldenticket.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.dazzi.goldenticket.adapter.TutorialSliderAdapter
 import kotlinx.android.synthetic.main.activity_tutorial.*
