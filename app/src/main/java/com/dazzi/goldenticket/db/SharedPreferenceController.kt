789061dc4d4628b6d2ec7487fc0d317b39653170
ganghee <idiosauri@naver.com>
2019-08-09 23:50:53 +0900

    Implements the clean code

--- app/src/main/java/com/dazzi/goldenticket/db/SharedPreferenceController.kt
+++ app/src/main/java/com/dazzi/goldenticket/db/SharedPreferenceController.kt
@@ -6,11 +6,11 @@ import android.content.SharedPreferences
 
 object SharedPreferenceController {
 
-    val MY_ACCOUNT = "unique_string"
+    private const val ACCOUNT = "unique_string"
 
     //유저 정보를 저장한다.
     fun setUserInfo(ctx: Context, userData: com.dazzi.goldenticket.data.UserData) {
-        val preference: SharedPreferences = ctx.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
         val editor: SharedPreferences.Editor = preference.edit()
         editor.putString("u_name", userData.name)
         editor.putString("u_idx", userData.user_idx)
@@ -24,26 +24,26 @@ object SharedPreferenceController {
 
 
     fun getUserName(ctx: Context): String? {
-        val preference: SharedPreferences = ctx.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
         return preference.getString("u_name","")
     }
     fun getUserEmail(ctx: Context): String? {
-        val preference: SharedPreferences = ctx.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
         return preference.getString("u_email","")
     }
     fun getUserToken(ctx: Context): String {
-        val preference: SharedPreferences = ctx.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
         return preference.getString("u_token","")!!
     }
     fun getUserPhone(ctx: Context): String? {
-        val preference: SharedPreferences = ctx.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
         return preference.getString("u_phone","")
     }
 
 
     //유저 정보를 해제한다. (로그아웃)
     fun clearUserToken(ctx: Context) {
-        val preference: SharedPreferences = ctx.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
         val editor: SharedPreferences.Editor = preference.edit()
         editor.clear()
         editor.apply()
