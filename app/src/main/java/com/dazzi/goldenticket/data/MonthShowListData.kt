5db49cb60cc235f02aea8fc9c644cff21eaac198
ganghee <idiosauri@naver.com>
2019-08-11 17:45:27 +0900

    update for clean code

--- app/src/main/java/com/dazzi/goldenticket/data/MonthShowListData.kt
+++ app/src/main/java/com/dazzi/goldenticket/data/MonthShowListData.kt
@@ -1,8 +1,8 @@
 package com.dazzi.goldenticket.data
 
-data class CardListData (
-    var cardIdx: Int,
-    var imageUrl: String,
-    var title: String,
-    var category: String
+data class MonthShowListData (
+    val cardIdx: Int,
+    val imageUrl: String,
+    val title: String,
+    val category: String
 )
